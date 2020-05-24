d1fae583e1e13b1c8b01c3e81f29c8f292a92ab3
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2020-05-23 23:43:27 -0300

    simple

--- get.py
+++ get.py
@@ -1,7 +1,13 @@
 import urllib, requests
-import time, os, sys
+import time, os
+import pathlib
+import datetime
+
+here = str(pathlib.Path(__file__).parent.absolute())
+print(here)
 
 def getAll():
+    print(datetime.datetime.now())
     village_url = "https://br101.tribalwars.com.br/map/village.txt"
     village_data = urllib.parse.unquote_plus(requests.get(village_url).text)
     
@@ -15,45 +21,30 @@ def getAll():
     oda_data = urllib.parse.unquote_plus(requests.get(oda_url).text)
 
     t = str(time.time())
-    with open("tw/villages/" + t + ".txt", "w") as f:
+    with open(here + "/tw/villages/" + t + ".txt", "w") as f:
         f.write(village_data)
 
-    with open("tw/players/" + t + ".txt", "w") as f:
+    with open(here + "/tw/players/" + t + ".txt", "w") as f:
         f.write(player_data)
 
-    with open("tw/odd/" + t + ".txt", "w") as f:
+    with open(here + "/tw/odd/" + t + ".txt", "w") as f:
         f.write(odd_data)
 
-    with open("tw/oda/" + t + ".txt", "w") as f:
+    with open(here + "/tw/oda/" + t + ".txt", "w") as f:
         f.write(oda_data)
-
-
-def job():
-    print("I'm working...")
 
 if __name__ == "__main__":
 
-    if not os.path.exists("tw/villages/"):
-        os.makedirs("tw/villages/")
+    if not os.path.exists(here + "/tw/villages/"):
+        os.makedirs(here + "/tw/villages/")
 
-    if not os.path.exists("tw/players/"):
-        os.makedirs("tw/players/")
+    if not os.path.exists(here + "/tw/players/"):
+        os.makedirs(here + "/tw/players/")
 
-    if not os.path.exists("tw/odd/"):
-        os.makedirs("tw/odd/")
+    if not os.path.exists(here + "/tw/odd/"):
+        os.makedirs(here + "/tw/odd/")
 
-    if not os.path.exists("tw/oda/"):
-        os.makedirs("tw/oda/")
+    if not os.path.exists(here + "/tw/oda/"):
+        os.makedirs(here + "/tw/oda/")
     
     getAll()
-    
-    if "1" in sys.argv:
-        exit()
-
-    import schedule
-    schedule.every(2).hours.do(getAll)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-    print("Stop!")
